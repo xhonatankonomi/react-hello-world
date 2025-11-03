adb05e8f0df7a09c0fb35f993d4b8c9679fe5070
xhonatankonomi <xhoni8962@gmail.com>
2025-11-03 14:10:08 +0100

    Update button functionality to decrement count and adjust instructional text

--- react-hello-world/src/App.jsx
+++ react-hello-world/src/App.jsx
@@ -18,11 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count - 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Non modificare <code>src/App.jsx</code> e salva per provare l'HMR
         </p>
       </div>
       <p className="read-the-docs">
